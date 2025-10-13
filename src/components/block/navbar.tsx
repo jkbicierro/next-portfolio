1101064bd95dc01bc3af1a1d57d4b5298d8f2867
Jeck <zrkeimperial@gmail.com>
2025-10-14 00:21:38 +0800

    refactor: improve navbar component structure and styling for better readability

--- src/components/block/navbar.tsx
+++ src/components/block/navbar.tsx
@@ -9,112 +9,110 @@ import { motion, useScroll } from "motion/react";
 import { ModeToggle } from "../buttons/mode-toggle";
 
 const navigation = [
-    { name: "Home", href: "/", icon: <Home size={16} /> },
-    { name: "Research", href: "/research", icon: <FolderSearch size={16} /> },
-    { name: "About", href: "/about", icon: <CircleUser size={16} /> },
-    { name: "Contact", href: "/contact", icon: <AtSign size={16} /> },
+  { name: "Home", href: "/", icon: <Home size={16} /> },
+  { name: "Research", href: "/research", icon: <FolderSearch size={16} /> },
+  { name: "About", href: "/about", icon: <CircleUser size={16} /> },
+  { name: "Contact", href: "/contact", icon: <AtSign size={16} /> },
 ];
 
 export function NavBar() {
-    const pathname = usePathname();
-    const { scrollYProgress } = useScroll();
+  const pathname = usePathname();
+  const { scrollYProgress } = useScroll();
 
-    return (
-        <>
-            <div className="hidden lg:block">
-                <nav className="bg-background px-[30px] fixed h-[60px] w-full border-b flex items-center justify-around">
-                    <div className="flex items-center gap-3">
-                        <div className="absolute mt-[18px] ml-[18px] z-50 border-2 border-background bg-yellow-500 rounded-full h-[10px] w-[10px]"></div>
-                        <Avatar className="w-[25px] h-[25px]">
-                            <AvatarImage
-                                src="https://github.com/jkbicierro.png"
-                                alt="@shadcn"
-                            />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
+  return (
+    <>
+      <div className="hidden lg:block">
+        <nav className="z-[999] backdrop-filter backdrop-blur-md px-[30px] fixed h-[60px] w-full flex items-center justify-around">
+          <div className="flex items-center gap-3">
+            <div className="absolute mt-[18px] ml-[18px] z-50 border-2 border-background bg-yellow-500 rounded-full h-[10px] w-[10px]"></div>
+            <Avatar className="w-[25px] h-[25px]">
+              <AvatarImage
+                src="https://github.com/jkbicierro.png"
+                alt="@shadcn"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
 
-                        <span className="text-sm">John Bicierro</span>
-                    </div>
+            <span className="text-sm">John Bicierro</span>
+          </div>
 
-                    <div className="flex items-center gap-8">
-                        <ul className="text-zinc-400 flex gap-8 text-[12px] uppercase font-bold">
-                            {navigation.map((item, index) => {
-                                const isActive = pathname === item.href;
-                                return (
-                                    <Link key={index} href={item.href}>
-                                        <li
-                                            className={`${
-                                                isActive
-                                                    ? "text-blue-500 border-b-2 border-blue-500"
-                                                    : "text-slate-500"
-                                            }`}
-                                        >
-                                            {item.name}
-                                        </li>
-                                    </Link>
-                                );
-                            })}
-                        </ul>
-                        <div className="flex gap-2">
-                            <Button>
-                                <Sparkles />
-                                Unlock Resources
-                            </Button>
-                            <ModeToggle />
-                        </div>
-                    </div>
-                </nav>
-                <div className="h-[60px]"></div>
-                <motion.div
-                    id="scroll-indicator"
-                    style={{
-                        scaleX: scrollYProgress,
-                        position: "fixed",
-                        top: 60,
-                        left: 0,
-                        right: 0,
-                        height: 2,
-                        originX: 0,
-                    }}
-                    className="bg-blue-500 z-50"
-                />
+          <div className="flex items-center gap-8">
+            <ul className="text-zinc-400 flex gap-8 text-[12px] uppercase font-bold">
+              {navigation.map((item, index) => {
+                const isActive = pathname === item.href;
+                return (
+                  <Link key={index} href={item.href}>
+                    <li
+                      className={`${
+                        isActive
+                          ? "text-blue-500 border-b-2 border-blue-500"
+                          : "text-slate-500"
+                      }`}
+                    >
+                      {item.name}
+                    </li>
+                  </Link>
+                );
+              })}
+            </ul>
+            <div className="flex gap-2">
+              <Button>
+                <Sparkles />
+                Unlock Resources
+              </Button>
+              <ModeToggle />
             </div>
+          </div>
+        </nav>
+        <div className="h-[60px]"></div>
+        <motion.div
+          id="scroll-indicator"
+          style={{
+            scaleX: scrollYProgress,
+            position: "fixed",
+            top: 60,
+            left: 0,
+            right: 0,
+            height: 2,
+            originX: 0,
+          }}
+          className="bg-blue-500 z-50"
+        />
+      </div>
 
-            <div className="block lg:hidden">
-                <nav className="bg-background h-[50px] fixed bottom-0 border-t w-full flex items-center justify-center">
-                    <ul className="w-full flex justify-around">
-                        {navigation.map((item, index) => {
-                            const isActive = pathname === item.href;
-                            return (
-                                <Link key={index} href={item.href}>
-                                    <li
-                                        className={`${
-                                            isActive && "text-blue-500"
-                                        } 
+      <div className="block lg:hidden">
+        <nav className="bg-background h-[50px] fixed bottom-0 border-t w-full flex items-center justify-center">
+          <ul className="w-full flex justify-around">
+            {navigation.map((item, index) => {
+              const isActive = pathname === item.href;
+              return (
+                <Link key={index} href={item.href}>
+                  <li
+                    className={`${isActive && "text-blue-500"} 
                                             flex flex-col text-[6px] gap-1 uppercase font-semibold items-center`}
-                                    >
-                                        {item.icon}
-                                        {item.name}
-                                    </li>
-                                </Link>
-                            );
-                        })}
-                    </ul>
-                </nav>
-                <motion.div
-                    id="scroll-indicator"
-                    style={{
-                        scaleX: scrollYProgress,
-                        position: "fixed",
-                        bottom: 50,
-                        left: 0,
-                        right: 0,
-                        height: 2,
-                        originX: 0,
-                    }}
-                    className="bg-blue-500 z-50"
-                />
-            </div>
-        </>
-    );
+                  >
+                    {item.icon}
+                    {item.name}
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </nav>
+        <motion.div
+          id="scroll-indicator"
+          style={{
+            scaleX: scrollYProgress,
+            position: "fixed",
+            bottom: 50,
+            left: 0,
+            right: 0,
+            height: 2,
+            originX: 0,
+          }}
+          className="bg-blue-500 z-50"
+        />
+      </div>
+    </>
+  );
 }
