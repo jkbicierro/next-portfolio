c0e14fca422cdc601516aa9cddb7cf94759c1919
Jeck <zrkeimperial@gmail.com>
2025-10-14 00:21:26 +0800

    feat: add metadata for Contact page to enhance SEO and user engagement

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact - jkbicierro",
+  description:
+    "Get in touch with John Bicierro. Whether you have a question, feedback, or just want to connect, feel free to reach out.",
+};
 
 export default function Page() {
-  return (
-    <div>page</div>
-  )
+  return <div>page</div>;
 }
