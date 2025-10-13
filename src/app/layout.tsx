b0af546c164740b4479330a6382374e33f27cb29
Jeck <zrkeimperial@gmail.com>
2025-10-14 00:20:47 +0800

    fix: update layout metadata and improve code formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,41 +7,41 @@ import Copyright from "@/components/block/copyright";
 import Footer from "@/components/block/footer";
 
 const dm_font = DM_Sans({
-    variable: "--font-dm",
-    subsets: ["latin"],
+  variable: "--font-dm",
+  subsets: ["latin"],
 });
 
 const pacifico_font = Pacifico({
-    weight: "400",
-    variable: "--font-pacifico",
-    subsets: ["latin"],
+  weight: "400",
+  variable: "--font-pacifico",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Jeck",
-    description: "Portfolio",
+  title: "John Bicierro - jkbicierro",
+  description: "John Bicierro's personal portfolio website.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={`${dm_font.variable} ${pacifico_font.variable}`}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="dark"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    <NavBar />
-                    {children}
-                    <Copyright />
-                    <Footer />
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${dm_font.variable} ${pacifico_font.variable}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar />
+          {children}
+          <Copyright />
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
