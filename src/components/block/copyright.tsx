439cc6061bf501c77a06e26f8c9a0562ea0d6b90
Jeck <zrkeimperial@gmail.com>
2025-12-19 10:37:12 +0800

    style: format code for improved readability in copyright component

--- src/components/block/copyright.tsx
+++ src/components/block/copyright.tsx
@@ -4,18 +4,18 @@ import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 export default function copyright() {
-    return (
-        <div className="hidden lg:block">
-            <span className="flex gap-2 items-center fixed z-50 bottom-3 left-3 text-slate-500 text-xs">
-                <Avatar className="w-[12px] h-[12px]">
-                    <AvatarImage
-                        src="https://github.com/jkbicierro.png"
-                        alt="@shadcn"
-                    />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                Crafted from Scratch &copy; 2025 John Bicierro
-            </span>
-        </div>
-    );
+  return (
+    <div className="hidden lg:block">
+      <span className="flex gap-2 items-center fixed z-50 bottom-3 left-3 text-slate-500 text-xs">
+        <Avatar className="w-[12px] h-[12px]">
+          <AvatarImage
+            src="https://github.com/jkbicierro.png"
+            alt="John Bicierro"
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        Crafted from Scratch &copy; 2025 John Bicierro
+      </span>
+    </div>
+  );
 }
