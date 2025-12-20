6e6594cdde66167a8c5ce7fd6cca43a9becad7d1
Jeck <zrkeimperial@gmail.com>
2025-12-20 20:10:20 +0800

    feat: add dynamic project page with metadata generation and content rendering

---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import type { Metadata } from "next";
+import MDXRenderer from "@/components/mdx/mdx-renderer";
+import { notFound } from "next/navigation";
+
+type PageProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const filePath = path.join(process.cwd(), "src/content/projects", `${slug}.mdx`);
+  const file = fs.readFileSync(filePath, "utf8");
+  const { data } = matter(file);
+
+  return {
+    title: data.title + " - John Bicierro" || "Projects - John Bicierro",
+    description:
+      data.description ||
+      "A collection of projects showcasing my work in web development, design, and technology. Explore case studies, insights, and innovative solutions I've crafted to solve real-world problems.",
+  };
+}
+
+export default async function Page({ params }: PageProps) {
+  const { slug } = await params;
+  const filePath = path.join(process.cwd(), "src/content/projects", `${slug}.mdx`);
+
+  try {
+    const file = fs.readFileSync(filePath, "utf8");
+    const { content } = matter(file);
+
+    return (
+      <>
+        <section className="border py-[200px] flex justify-center">
+          <div className="w-[650px] border">
+            <MDXRenderer source={content} />
+          </div>
+        </section>
+      </>
+    );
+  } catch {
+    return notFound();
+  }
+}
