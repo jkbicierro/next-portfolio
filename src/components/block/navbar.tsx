1353fd023aaa18b545ec69d84e9f21feb8f64249
Jeck <zrkeimperial@gmail.com>
2025-12-20 21:15:04 +0800

    feat: refactor NavBar component for improved structure and readability

--- src/components/block/navbar.tsx
+++ src/components/block/navbar.tsx
@@ -20,64 +20,64 @@ export function NavBar() {
 
   return (
     <>
-      <div className="">
-        <nav className="z-[999] backdrop-filter backdrop-blur-md px-[30px] fixed h-[60px] w-full flex items-center justify-between lg:justify-around">
-          <div className="flex items-center gap-3">
-            <div className="absolute mt-[18px] ml-[18px] z-50 border-2 border-background bg-yellow-500 rounded-full h-[10px] w-[10px]"></div>
-            <Avatar className="w-[25px] h-[25px]">
-              <AvatarImage src="https://github.com/jkbicierro.png" alt="John Bicierro" />
-              <AvatarFallback>JB</AvatarFallback>
-            </Avatar>
+      {/* style:lg */}
+      <nav className="z-[999] backdrop-filter backdrop-blur-md px-[30px] fixed h-[60px] w-full flex items-center justify-between lg:justify-around">
+        <div className="flex items-center gap-3">
+          <div className="absolute mt-[18px] ml-[18px] z-50 border-2 border-background bg-yellow-500 rounded-full h-[10px] w-[10px]"></div>
+          <Avatar className="w-[25px] h-[25px]">
+            <AvatarImage src="https://github.com/jkbicierro.png" alt="John Bicierro" />
+            <AvatarFallback>JB</AvatarFallback>
+          </Avatar>
 
-            <span className="text-sm">John Bicierro</span>
+          <span className="text-sm">John Bicierro</span>
+        </div>
+
+        <div className="flex items-center gap-8">
+          <div className="hidden lg:block">
+            <ul className="text-zinc-400 flex gap-8 text-[12px] uppercase font-bold">
+              {navigation.map((item, index) => {
+                const isActive = pathname === item.href;
+                return (
+                  <li
+                    key={index}
+                    className={`${
+                      isActive
+                        ? "text-blue-500 border-b-2 border-blue-500"
+                        : "text-slate-500"
+                    }`}
+                  >
+                    <Link href={item.href}>{item.name}</Link>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
 
-          <div className="flex items-center gap-8">
-            <div className="hidden lg:block">
-              <ul className="text-zinc-400 flex gap-8 text-[12px] uppercase font-bold">
-                {navigation.map((item, index) => {
-                  const isActive = pathname === item.href;
-                  return (
-                    <li
-                      key={index}
-                      className={`${
-                        isActive
-                          ? "text-blue-500 border-b-2 border-blue-500"
-                          : "text-slate-500"
-                      }`}
-                    >
-                      <Link href={item.href}>{item.name}</Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-
-            <div className="flex gap-2">
-              {/* <Button>
+          <div className="flex gap-2">
+            {/* <Button>
                 <Sparkles />
                 Unlock Resources
               </Button> */}
-              <ModeToggle />
-            </div>
+            <ModeToggle />
           </div>
-        </nav>
-        <div className="h-[60px]"></div>
-        <motion.div
-          id="scroll-indicator"
-          style={{
-            scaleX: scrollYProgress,
-            position: "fixed",
-            top: 60,
-            left: 0,
-            right: 0,
-            height: 2,
-            originX: 0,
-          }}
-          className="bg-blue-500 z-50"
-        />
-      </div>
+        </div>
+      </nav>
+      <div className="h-[60px]"></div>
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 60,
+          left: 0,
+          right: 0,
+          height: 2,
+          originX: 0,
+        }}
+        className="bg-blue-500 z-50"
+      />
 
+      {/* style:xs,sm,base */}
       <div className="block lg:hidden">
         <nav className="bg-background h-[50px] fixed bottom-0 border-t w-full flex items-center justify-center">
           <ul className="w-full flex justify-around">
