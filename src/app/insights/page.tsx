272cd261bdaaa6b9e3a3c404c96a96f689696451
Jeck <zrkeimperial@gmail.com>
2025-12-20 15:09:26 +0800

    feat: implement insights page to display articles with date and description

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Metadata } from "next";
+import { getAllInsights } from "@/lib/insights";
+import { format, parseISO } from "date-fns";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Insights - John Bicierro",
@@ -8,5 +11,32 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <div>page</div>;
+  const insights = getAllInsights();
+
+  console.log(insights);
+  return (
+    <>
+      <section className="py-[200px] flex flex-col items-center">
+        <h2>I share my strategies, learnings, and practical approaches</h2>
+
+        <div className="mt-20 flex flex-col gap-10">
+          {insights.map((insight) => (
+            <Link key={insight.slug} href={`/insights/${insight.slug}`}>
+              <div className="flex flex-row gap-5">
+                <small className="w-[150px] text-slate-600 dark:text-slate-400">
+                  {insight.date ? format(parseISO(insight.date), "MMMM d, yyyy") : ""}
+                </small>
+                <div className="flex flex-col gap-2">
+                  <h5>{insight.title}</h5>
+                  <small className="text-slate-600 dark:text-slate-400">
+                    {insight.description}
+                  </small>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
