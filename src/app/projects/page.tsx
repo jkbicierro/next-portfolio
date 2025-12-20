8c045e021469ff4cbc50b4f806f11a658666bd5d
Jeck <zrkeimperial@gmail.com>
2025-12-20 13:36:35 +0800

    feat: add projects page with metadata and basic structure

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Projects - John Bicierro",
+  description:
+    "A showcase of projects developed by John Bicierro, highlighting skills and expertise.",
+};
+
+export default function Page() {
+  return <div>page</div>;
+}
