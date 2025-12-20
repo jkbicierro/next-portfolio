eaaf7c24ab77a38610139e01028b5bb745f8093d
Jeck <zrkeimperial@gmail.com>
2025-12-20 13:36:26 +0800

    feat: add insights page with metadata and basic structure

---
+++ src/app/insights/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Insights - John Bicierro",
+  description:
+    "A collection of insightful articles and blog posts by John Bicierro, sharing knowledge and experiences.",
+};
+
+export default function Page() {
+  return <div>page</div>;
+}
