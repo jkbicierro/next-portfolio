b69b361bac2a159105efd313ec5e0d29d1e55db5
Jeck <zrkeimperial@gmail.com>
2025-12-20 13:36:01 +0800

    feat: implement footer component with navigation and social links

--- src/components/block/footer.tsx
+++ src/components/block/footer.tsx
@@ -1,5 +1,57 @@
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import Link from "next/link";
 
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="w-full border flex justify-center">
+      <div className="py-[50px] w-[1000px]">
+        <div className="flex justify-between">
+          <Avatar className="w-[25px] h-[25px]">
+            <AvatarImage
+              src="https://github.com/jkbicierro.png"
+              alt="John Bicierro"
+            />
+            <AvatarFallback>JB</AvatarFallback>
+          </Avatar>
+
+          {/* Navigation */}
+          <div className="flex flex-col gap-2">
+            <span>John Bicierro</span>
+            <Link href="#">Home</Link>
+            <Link href="#">Research</Link>
+            <Link href="#">Sitemap</Link>
+          </div>
+
+          {/* Resources */}
+          <div className="flex flex-col gap-2">
+            <span>Resources</span>
+            <Link href="#">
+              <small>Resume</small>
+            </Link>
+            <Link href="#">
+              <small>Curriculum Vitae</small>
+            </Link>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Link
+              href="https://www.linkedin.com/in/jkbicierro/"
+              target="_blank"
+            >
+              <FaLinkedin size={25} />
+            </Link>
+            <Link href="https://github.com/jkbicierro" target="_blank">
+              <FaGithubSquare size={25} />
+            </Link>
+            <a href="mailto:jkbicierro@gmail.com">
+              <MdEmail size={25} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
