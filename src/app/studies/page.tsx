00d58356735676cd34b66b900809b8b50e72d3a8
Jeck <zrkeimperial@gmail.com>
2025-12-20 15:37:10 +0800

    feat: implement case studies section with dynamic card layout and metadata

--- src/app/studies/page.tsx
+++ src/app/studies/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Case Studies - John Bicierro",
@@ -8,5 +9,69 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <div>page</div>;
+  return (
+    <main className="px-[20px]">
+      <section className="py-[200px] flex flex-col items-center">
+        <h2>see how i solve real problems and deliver results</h2>
+
+        <div className="mt-20 w-full lg:w-[1000px]">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+            {/* Cards */}
+            <div className="w-full">
+              <Image
+                src={"/assets/liquid/1.png"}
+                height={800}
+                width={800}
+                alt="Liquid project"
+                className="border rounded-xl h-[200px] w-full"
+              />
+
+              <h6 className="mt-4 truncate">Title</h6>
+
+              <div className="mt-2 flex items-center gap-2">
+                <small>Case Study</small>
+                <small className="text-slate-400">January 24, 2025</small>
+              </div>
+            </div>
+
+            {/* Cards */}
+            <div className="w-full">
+              <Image
+                src={"/assets/liquid/1.png"}
+                height={800}
+                width={800}
+                alt="Liquid project"
+                className="border rounded-xl h-[200px] w-full"
+              />
+
+              <h6 className="mt-4 truncate">Title</h6>
+
+              <div className="mt-2 flex items-center gap-2">
+                <small>Case Study</small>
+                <small className="text-slate-400">January 24, 2025</small>
+              </div>
+            </div>
+
+            {/* Cards */}
+            <div className="w-full">
+              <Image
+                src={"/assets/liquid/1.png"}
+                height={800}
+                width={800}
+                alt="Liquid project"
+                className="border rounded-xl h-[200px] w-full"
+              />
+
+              <h6 className="mt-4 truncate">Title</h6>
+
+              <div className="mt-2 flex items-center gap-2">
+                <small>Case Study</small>
+                <small className="text-slate-400">January 24, 2025</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
