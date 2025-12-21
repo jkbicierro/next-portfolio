23d50dae03597df93f97336081b8ab3009774d1c
Jeck <zrkeimperial@gmail.com>
2025-12-21 12:31:42 +0800

    feat: enhance case study page with reading time, improved layout, and navigation buttons

--- src/app/studies/[slug]/page.tsx
+++ src/app/studies/[slug]/page.tsx
@@ -5,6 +5,11 @@ import type { Metadata } from "next";
 import MDXRenderer from "@/components/mdx/mdx-renderer";
 import { notFound } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import readingTime from "reading-time";
+import { format, parseISO } from "date-fns";
+import { ChevronLeft, Share } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 type PageProps = {
   params: Promise<{ slug: string }>;
@@ -32,32 +37,59 @@ export default async function Page({ params }: PageProps) {
     const file = fs.readFileSync(filePath, "utf8");
     const { content, data } = matter(file);
 
+    const timeRead = readingTime(content);
+
     return (
-      <>
-        <section className="py-[200px] flex flex-col items-center ">
-          <div className="flex flex-col gap-3">
-            {/* Header */}
-            <div className="flex gap-3 items-center">
-              <Avatar className="w-[25px] h-[25px]">
-                <AvatarImage
-                  src="https://github.com/jkbicierro.png"
-                  alt="John Bicierro"
-                />
-                <AvatarFallback>JB</AvatarFallback>
-              </Avatar>
-              <span className="text-sm">John Bicierro</span>
+      <main className="px-[20px]">
+        <section className="py-[100px] flex flex-col items-center">
+          <div className="w-[1000px]">
+            <div className="flex flex-col items-start gap-3">
+              {/* Button */}
+              <div className="w-full flex justify-between">
+                <Button variant="link" className="mb-5 flex items-center p-0" asChild>
+                  <Link href="/studies">
+                    <ChevronLeft size={16} />
+                    Back to case studies
+                  </Link>
+                </Button>
+                <Button variant={"outline"}>
+                  <Share size={16} /> Share "{data.title}"
+                </Button>
+              </div>
+
+              {/* Header */}
+              <div className="mb-5 flex gap-3 items-center">
+                <Avatar className="w-[25px] h-[25px]">
+                  <AvatarImage
+                    src="https://github.com/jkbicierro.png"
+                    alt="John Bicierro"
+                  />
+                  <AvatarFallback>JB</AvatarFallback>
+                </Avatar>
+                <span className="text-sm">John Bicierro</span>
+              </div>
+
+              {/* Stats */}
+              <time
+                dateTime={data.date}
+                className="text-slate-600 dark:text-slate-400 uppercase text-xs"
+              >
+                {data.date ? format(parseISO(data.date), "MMMM d, yyyy") : ""}
+              </time>
+              <h2>{data.title}</h2>
+              <p className="text-slate-600 dark:text-slate-400">{data.description}</p>
+              <small className="text-slate-600 dark:text-slate-400 uppercase text-xs">
+                {data.type} · {timeRead.text}
+              </small>
             </div>
 
-            <h2>{data.title}</h2>
-            <p>{data.description}</p>
-            <small className="uppercase text-xs">{data.type} · 12 mins read</small>
-          </div>
-
-          <div className="mt-20">
-            <MDXRenderer source={content} />
+            {/* Content */}
+            <div className="mt-10">
+              <MDXRenderer source={content} />
+            </div>
           </div>
         </section>
-      </>
+      </main>
     );
   } catch {
     return notFound();
